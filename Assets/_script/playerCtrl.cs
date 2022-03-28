4dfb5ba9dddaee5c98feb783818a627ac5e74f4c
Rifat <sad@happy.com>
2022-03-29 05:32:37 +0600

    Initial Commit | Depricated Project

---
+++ Assets/_script/playerCtrl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCtrl : MonoBehaviour
+{
+    public float move_speed;
+    public float jump_force;
+
+    Rigidbody RB;
+
+    void Start()
+    {
+        RB = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * move_speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            RB.AddForce(Vector3.up * jump_force * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+}
